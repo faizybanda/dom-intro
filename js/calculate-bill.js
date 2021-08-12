97d04c8fe06d7efc82188591ebbae147bc2b778e
Faizal <faizy.banda@gmail.com>
2021-08-12 10:45:21 +0200

    initial commit

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -11,3 +11,63 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+
+/*const billStringElement = document.querySelector(".billString");
+
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringElement.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+    
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringField.value;   
+    //round to two decimals
+    var roundedBillTotal = totalPhoneBill(billString);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+
+calculateBtn.addEventListener('click', calculateBtnClicked);
+*/
+
+var calculateBtn = document.querySelector('.calculateBtn');
+var billTotalElem = document.querySelector('.billTotal');
+var billString = document.querySelector('.billString');
+
+var billTotal = 0;
+
+function totalBill(){
+  var billElements = billString.value.split(",");
+  billTotal = 0;
+  billElements.forEach(function(element){
+    var trimmedElement = element.trim();
+    if (trimmedElement === "call"){
+      billTotal += 2.75
+    }
+    else if (trimmedElement === "sms"){
+      billTotal += 0.75;
+    }
+  });
+
+  billTotalElem.innerHTML = billTotal.toFixed(2);
+}
+
+calculateBtn.addEventListener("click", totalBill);
--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,41 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+var addToBillBtn = document.querySelector('.addToBillBtn');
+var billTypeText = document.querySelector('.billTypeText');
+var callTotalOneElem = document.querySelector('.callTotalOne');
+var smsTotalOneElem = document.querySelector('.smsTotalOne');
+var totalOneElem = document.querySelector('.totalOne');
+
+var callsTotal = 0;
+var smsTotal = 0;
+
+addToBillBtn.addEventListener('click', function(){
+
+    
+    // get the value entered in the billType textfield
+    var billTypeEntered = billTypeText.value;
+  
+    // update the correct total
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
+    //update the totals that is displayed on the screen.
+    callTotalOneElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalOneElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalOneElem.innerHTML = totalCost.toFixed(2);
+
+
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalOneElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalOneElem.classList.add("warning");
+    }
+});
